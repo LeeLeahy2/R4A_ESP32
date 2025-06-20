2d739431075c2491c8994d6b4a9580d5973a31a3
Lee Leahy <lpleahyjr@gmail.com>
2025-06-19 14:55:55 -1000

    Ex 08: Add I2C menu

--- examples/08_LED_Matrix_Display/Menu.ino
+++ examples/08_LED_Matrix_Display/Menu.ino
@@ -9,6 +9,7 @@
 enum MENU_TABLE_INDEX
 {
     MTI_NVM = R4A_MENU_MAIN + 1,
+    MTI_I2C = R4A_MENU_MAIN + 1,
     // Add new menu values before this line
     MTI_MAX
 };
@@ -18,6 +19,7 @@ const int menuTableIndexMax = MTI_MAX;
 const R4A_MENU_ENTRY mainMenuTable[] =
 {
     // Command  menuRoutine         menuParam       HelpRoutine align   HelpText
+    {"I2C",     nullptr,            MTI_I2C,        nullptr,    0,      "Enter the I2C menu"},
     {"nvm",     nullptr,            MTI_NVM,        nullptr,    0,      "Enter the NVM menu"},
     {"r",  r4aEsp32MenuSystemReset, 0,              nullptr,    0,      "System reset"},
     {"x",       nullptr,            R4A_MENU_NONE,  nullptr,    0,      "Exit the menu system"},
@@ -28,6 +30,7 @@ const R4A_MENU_TABLE menuTable[] =
 {
     // menuName         preMenu routine firstEntry          entryCount
     {"Main Menu",       nullptr,        mainMenuTable,      MAIN_MENU_ENTRIES},
+    {"I2C Menu",        nullptr,        r4aI2cMenuTable,    R4A_I2C_MENU_ENTRIES},
     {"NVM Menu",        nullptr,      r4aEsp32NvmMenuTable, R4A_ESP32_NVM_MENU_ENTRIES},
 };
 const int menuTableEntries = sizeof(menuTable) / sizeof(menuTable[0]);
