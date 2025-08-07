5dc2e8eba814a756b95ceda59345e6639f0e36e5
Lee Leahy <lpleahyjr@gmail.com>
2025-08-06 16:21:57 -1000

    Freenove 4WD Car: Switch to using R4A_4WD_CAR_SPI_WS2812_* macros

--- examples/Freenove_4WD_Car/Freenove_4WD_Car.ino
+++ examples/Freenove_4WD_Car/Freenove_4WD_Car.ino
@@ -296,28 +296,7 @@ USE_SERVO_TABLE;
 // SPI support - WS2812 LEDs
 //****************************************
 
-// SPI controller connected to the SPI bus
-R4A_ESP32_SPI_CONTROLLER spiBus =
-{
-    {
-        1,      // SPI bus number
-        -1,     // SCLK GPIO
-        BATTERY_WS2812_PIN,   // MOSI GPIO
-        -1,     // MISO GPIO
-        r4aEsp32SpiTransfer // SPI transfer routine
-    },
-    nullptr,
-};
-
-// WS2812 - RGB LED connected to the SPI bus
-const R4A_SPI_DEVICE ws2812 =
-{
-    &spiBus._spiBus, // SPI bus
-    4 * 1000 * 1000, // Clock frequency
-    -1,              // No chip select pin
-    0,               // Clock polarity
-    0,               // Clock phase
-};
+R4A_4WD_CAR_SPI_WS2812_GLOBALS;
 
 //****************************************
 // Web server
@@ -411,6 +390,9 @@ void setup()
         r4aReportFatalError("Failed to create the core 0 task!");
     log_v("Core 0 task started");
 
+    // Initialize the SPI controller and WS2812 LEDs
+    R4A_4WD_CAR_SPI_WS2812_SETUP(1);
+
     // Start WiFi if enabled
     log_v("Calling r4aWifiBegin");
     r4aWifiBegin();
@@ -424,31 +406,6 @@ void setup()
 
     // Enable web server debugging
     r4aWebServerDebug = webServerDebug ? &Serial : nullptr;
-
-    // Initialize the SPI controller
-    log_v("r4aEsp32SpiBegin");
-    if (!r4aEsp32SpiBegin(&spiBus))
-        r4aReportFatalError("Failed to initialize the SPI controller!");
-
-    // Select the WS2812 devices on the SPI bus
-    if (!r4aEsp32SpiDeviceSelect(&ws2812))
-        r4aReportFatalError("Failed to select the WS2812 devices on the SPI bus!");
-
-    // Initialize the SPI controller for the WD2812 LEDs
-    if (!r4aLEDSetup(&ws2812, car.numberOfLEDs))
-        r4aReportFatalError("Failed to allocate the SPI device for the WS2812 LEDs!");
-
-    // Turn off all of the 3 color LEDs
-    log_v("Calling car.ledsOff");
-    car.ledsOff();
-
-    // Reduce the LED intensity
-    log_v("Calling r4aLEDSetIntensity");
-    r4aLEDSetIntensity(1);
-
-    // Set the initial LED values
-    log_v("Calling r4aLEDUpdate");
-    r4aLEDUpdate(true);
 
     // Allocate the loop buffers
     uint32_t length = sizeof(R4A_TIME_USEC_t) * LOOP_CORE_1_TIME_ENTRIES;
